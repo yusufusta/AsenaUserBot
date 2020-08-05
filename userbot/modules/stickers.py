72569f92d7baaabeef34cd3e87f83c89265ac9d6
Quiec <46392667+Quiec@users.noreply.github.com>
2020-08-05 20:59:02 +0300

    asena: stickers: some fixes

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -142,6 +142,8 @@ async def kang(event):
                     await bot.send_file("@ezstickerbot", message, force_document=True)
                 except YouBlockedUserError:
                     return await event.edit("`Lütfen` @EzStickerBot `engelini açın ve tekrar deneyin!`")
+                except:
+                    await bot.send_file("@EzStickerBotBackupBot", message, force_document=True)
 
                 response = await conv.wait_event(events.NewMessage(incoming=True,from_users=350549033))
                 await bot.send_read_acknowledge(350549033)
@@ -224,27 +226,10 @@ async def newpack(is_anim, sticker, emoji, packtitle, packname, message):
 async def resize_photo(photo):
     """ Resize the given photo to 512x512 """
     image = Image.open(photo)
-    maxsize = (512, 512)
-    if (image.width and image.height) < 512:
-        size1 = image.width
-        size2 = image.height
-        if image.width > image.height:
-            scale = 512 / size1
-            size1new = 512
-            size2new = size2 * scale
-        else:
-            scale = 512 / size2
-            size1new = size1 * scale
-            size2new = 512
-        size1new = math.floor(size1new)
-        size2new = math.floor(size2new)
-        sizenew = (size1new, size2new)
-        image = image.resize(sizenew)
-    else:
-        image.thumbnail(maxsize)
-
+    scale = 512 / max(image.width, image.height)
+    new_size = (int(image.width*scale), int(image.height*scale))
+    image = image.resize(new_size, Image.ANTIALIAS)
     return image
-
 
 CMD_HELP.update({
     "stickers":
